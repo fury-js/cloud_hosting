fdd605df1224602287c9a5e7c673225b2a03661b
fury-js <chukkyc582@gmail.com>
2022-08-02 02:18:56 +0200

    build footer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
+    // console.log("clicked");
     this.classList.toggle("collapsible--expanded");
   })
 );
